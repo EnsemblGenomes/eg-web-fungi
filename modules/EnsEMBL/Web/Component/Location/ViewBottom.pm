aafdecb26a3cec0eb8541645480e3db73dcd2498
Julia Khobova <jk@ebi.ac.uk>
2014-07-11 17:04:48 +0100

    Allow to zoom out the bottom image according to  a new setting in the ini file.

--- modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
+++ modules/EnsEMBL/Web/Component/Location/ViewBottom.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,31 +18,24 @@ limitations under the License.
 
 package EnsEMBL::Web::Component::Location::ViewBottom;
 
-use strict;
-use warnings;
-no warnings 'uninitialized';
-
-use Time::HiRes qw(time);
-
-
 sub content {
   my $self        = shift;
   my $hub         = $self->hub;
   my $object      = $self->object;
-  my $threshold   = 1000100 * ($hub->species_defs->ENSEMBL_GENOME_SIZE || 1);
+  my $threshold   = 1000100 * ($hub->species_defs->ENSEMBL_MAX_ZOOM_OUT_LEVEL || $hub->species_defs->ENSEMBL_GENOME_SIZE || 1);
+
   my $image_width = $self->image_width;
-
+  
   return $self->_warning('Region too large', '<p>The region selected is too large to display in this view - use the navigation above to zoom in...</p>') if $object->length > $threshold;
-
+ 
   my $slice        = $object->slice;
-  my $length       = $slice->length();
+  my $length       = $slice->end - $slice->start + 1;
   my $image_config = $hub->get_imageconfig('contigviewbottom');
-  my $s            = $hub->get_viewconfig('ViewTop')->get('show_panel') eq 'yes' ? 3 : 2;
-
+  
   $image_config->set_parameters({
     container_width => $length,
     image_width     => $image_width || 800, # hack at the moment
-    slice_number    => "1|$s"
+    slice_number    => '1|3'
   });
 
   ## Force display of individual low-weight markers on pages linked to from Location/Marker
@@ -52,29 +45,21 @@ sub content {
       { marker_id => $marker_id }
     );
   }
-
-  # Add multicell configuration                                                                                                                                                                                
-  if (keys %{$hub->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}}){
-      my $web_slice_obj = $self->new_object( 'Slice', $slice, $object->__data );
-      my $cell_line_data = $web_slice_obj->get_cell_line_data($image_config);
-      $image_config->{'data_by_cell_line'} = $cell_line_data;
-  }
-
-  # Lets see if we have any das sources                                                                                                                                                                         
-  $self->_attach_das($image_config);
-
+  
+  # Add multicell configuration
+  $image_config->{'data_by_cell_line'} = $self->new_object('Slice', $slice, $object->__data)->get_cell_line_data_closure($image_config) if keys %{$hub->species_defs->databases->{'DATABASE_FUNCGEN'}{'tables'}{'cell_type'}{'ids'}};
   $image_config->_update_missing($object);
-
+  
+  my $info  = $self->_add_object_track($image_config);
   my $image = $self->new_image($slice, $image_config, $object->highlights);
-
-  return if $self->_export_image($image);
-
+  
+	return if $self->_export_image($image);
+  
   $image->{'panel_number'} = 'bottom';
   $image->imagemap         = 'yes';
   $image->set_button('drag', 'title' => 'Click or drag to centre display');
-
-  return $image->render;
-
+  
+  return $info . $image->render;
 }
 
 
