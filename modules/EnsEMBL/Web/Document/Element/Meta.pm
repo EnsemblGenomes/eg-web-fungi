f5ed3eb455deeed31051d3cbbc16913c27933894
Anne Lyle <ens-ap5@users.noreply.github.com>
2022-08-25 15:37:15 +0100

    Remove keywords as they are obsolete

--- modules/EnsEMBL/Web/Document/Element/Meta.pm
+++ modules/EnsEMBL/Web/Document/Element/Meta.pm
@@ -21,7 +21,6 @@ package EnsEMBL::Web::Document::Element::Meta;
 sub init {
   my $self = shift;
   $self->add('description', 'Ensembl Fungi is a genome-centric portal for fungal species of scientific interest');
-  $self->add('keywords', 'Ensembl, Ensembl Genomes, genome, genome browser, comparative genomics, variation, SNPs, EST, mRNA, rna-Seq, orthologs, paralogs, synteny, assembly, genes, transcripts, translations, proteins, S. pombe, S. cerevisiae, fission yeast, bakers yeast, Saccharomyces, wheat rust, fungi');
 }
 
 1;
